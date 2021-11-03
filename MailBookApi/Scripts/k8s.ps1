c180faf5b97edcad78ebfa62f0d5367ed31e9899
arrak <arrakyambupah@bangkokbank.com>
2021-11-03 20:21:59 +0700

    Remove Startup Database Migration

---
+++ MailBookApi/Scripts/k8s.ps1
@@ -0,0 +1,8 @@
+# apply deployment file to k8s
+kubectl apply -f ../../deployment.yaml
+
+# this use for get all deployment in k8s
+# kubectl get deployments
+
+# delete deployment
+# kubectl delete deployment mailbook-api-depl
